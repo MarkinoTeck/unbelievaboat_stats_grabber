0901c788975be6923d70faf1e3b4233d9249c6a9
Roblox-Script <93880780+MarkinoTeck@users.noreply.github.com>
2022-08-11 21:32:09 +0200

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 from rich.console import Console
 from rich.table import Table
 import time
@@ -5,27 +6,39 @@ import json
 import requests
 import os
 
-my_stats = Table(title="Stats")
+
+# generate or get confing file
+if os.path.exists(os.getcwd() + "/config.json"):
+    with open("./config.json") as f:
+        configData = json.load(f)
+else:
+    configTemplate = {"server": 947666187653885982, "user": 722452978870779934, "clears": True}
+    with open(os.getcwd() + "/config.json", "w+") as f:
+        json.dump(configTemplate, f)
+
+
+#get var from the external file
+server = configData["server"]
+user = configData["user"]
+clears = configData["clears"]
+
+#vars ecc
+_stats = Table(title="Stats")
 console = Console()
-# collect all pets url
-url = "https://unbelievaboat.com/api/v1/guilds/947666187653885982/users/722452978870779934"
-headers = {"Accept": "application/json", "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOiIxMDA1MDk2ODQ3NjU3Nzk4MzkzIiwiaWF0IjoxNjU5NzA0MTYyfQ.bfTtQZcF0LEHiGqPaJJbVR8uONihcUf5KvDjo9XJy2w"}
+url = "https://unbelievaboat.com/api/v1/guilds/{}/users/{}".format(server, user)
+headers = {"Accept": "application/json", "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOiIxMDA3MzUxNjY4Njk5OTU3MDExIiwiaWF0IjoxNjYwMjQxNzU5fQ.7z4GDeAH3hTUoMvFDj-1THOooNSmSUos5qDOfIEcoKU"}
 
 
-
-#stats
-my_stats.add_column("Hand")
-my_stats.add_column("Bank")
-my_stats.add_column("Total")
-my_stats.add_column("Money Made")
-my_stats.add_column("Rank")
-my_stats.add_column("Betting")
-my_stats.add_column("Bet Color")
-my_stats.add_column("Starting Bet")
+#stats table
+_stats.add_column("Clock")
+_stats.add_column("Hand")
+_stats.add_column("Bank")
+_stats.add_column("Total")
+_stats.add_column("Money Made")
+_stats.add_column("Rank")
 
 
-
-# get api info
+#get api info
 def get_stats():
     response = requests.get(url, headers=headers)
     y = json.loads(response.text)
@@ -37,32 +50,40 @@ def get_stats():
     total = y["total"]
 
 
-# scegli bet iniziale
+#sets initial money
 get_stats()
 initial_money = total
 
+#loop
+while(True):
+
+  if clears == True: #resets the label
+    
+    _stats = Table(title="Stats")
+    _stats.add_column("Clock")
+    _stats.add_column("Hand")
+    _stats.add_column("Bank")
+    _stats.add_column("Total")
+    _stats.add_column("Money Made")
+    _stats.add_column("Rank")
 
 
-def main_():
- while(True):
   #clear
   os.system('cls' if os.name == 'nt' else 'clear')
 
-
   #update stats
   get_stats()
-  global made_money, rank
   made_money = total - initial_money
 
   #update label
+  _stats.add_row(datetime.datetime.now().strftime("%H:%M:%S"), 
+                  "{}$".format(f"{cash:,}"), 
+                  "{}$".format(f"{bank:,}"), 
+                  "{}$".format(f"{total:,}"), 
+                  "{}$".format(f"{made_money:,}"), 
+                  "#{}".format(rank)
+                )
 
-  my_stats.add_row("{}$".format(f"{cash:,}"), "{}$".format(f"{bank:,}"), "{}$".format(f"{total:,}"), "{}$".format(f"{made_money:,}"), "#{}".format(rank))
+  console.print(_stats)
 
-
-  console.print(my_stats)
-
-  time.sleep(30)
-
-
-# loops
-main_()
+  time.sleep(10)
